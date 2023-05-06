b454725e963c7038d5919ae2215aaefbc6ba8319
Will <evo721@protonmail.com>
2023-05-06 11:24:54 -0700

    optimization for the API route, to clear the serverless function timeout. split text more effectively with more efficient chunk sizes.

--- pages/api/summarize.js
+++ pages/api/summarize.js
@@ -4,6 +4,22 @@ import { OpenAI } from "langchain/llms/openai";
 import { loadSummarizationChain } from "langchain/chains";
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
 
+async function summarizeChunk(model, text) {
+  const chain = loadSummarizationChain(model);
+  const response = await chain.call({
+    input_documents: [text],
+  });
+  return response.text;
+}
+
+async function summarizeText(model, text, chunkSize = 2000) {
+  const textSplitter = new RecursiveCharacterTextSplitter({ chunkSize });
+  const docs = await textSplitter.createDocuments([text]);
+
+  const summaries = await Promise.all(docs.map((doc) => summarizeChunk(model, doc)));
+  return summaries.join(' ');
+}
+
 export default async function handler(req, res) {
   if (req.method === "POST") {
     try {
@@ -15,15 +31,9 @@ export default async function handler(req, res) {
       }
 
       const model = new OpenAI({ openAIApiKey: process.env.OPENAI_API_KEY, temperature: 0 });
-      const textSplitter = new RecursiveCharacterTextSplitter({ chunkSize: 1000 });
-      const docs = await textSplitter.createDocuments([text]);
+      const summary = await summarizeText(model, text);
 
-      const chain = loadSummarizationChain(model);
-      const response = await chain.call({
-        input_documents: docs,
-      });
-
-      res.status(200).json({ summary: response.text });
+      res.status(200).json({ summary });
     } catch (error) {
       console.error("Error in API route:", error);
       res.status(500).json({ error: error.message });
